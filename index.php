fdd32bdb24d68c8dab1ad754b528f0ca996785c6
Patrick Pineault <patrick-pineault@hotmail.com>
2024-06-07 09:47:18 -0400

    title

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if (isset($_GET['clearAll'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CookieCart</title>
+    <title>boutique paypal cookie </title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" defer></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" defer></script>
